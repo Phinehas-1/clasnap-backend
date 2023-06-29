c27388c587dcb8aef431ce8767e29531181e0481
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-06-29 12:56:55 +0100

    add project files

---
+++ src/main/java/com/bigdecimal/clasnapp/ClasnappApplication.java
@@ -0,0 +1,13 @@
+package com.bigdecimal.clasnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasnappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasnappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigdecimal/clasnapp/ClasnappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdecimal.clasnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasnappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
