78187b7818671e7b9063ec1dcaaaf39b7ce82fd1
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-07-05 08:41:25 +0100

    refactor: add JsonIgnore to leader prop in Group entity.
    also introduce a no-args constructor and constructor(GroupName)

--- src/main/java/com/bigdecimal/clasnapp/group/Group.java
+++ src/main/java/com/bigdecimal/clasnapp/group/Group.java
@@ -18,11 +18,13 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@NoArgsConstructor
 @Table(name = "_group")
 public class Group {
     @Id
@@ -35,9 +37,14 @@ public class Group {
     private GroupName name;
 
     @OneToOne
+    @JsonIgnore
     private User leader;
 
     @OneToMany(mappedBy = "group")
     @JsonIgnore
     private List<User> users = new ArrayList<User>();
+
+    public Group(GroupName name) {
+        this.name = name;
+    }
 }
