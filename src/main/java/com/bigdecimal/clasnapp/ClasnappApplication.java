cf0429e94ac8e5001c1b117690288c1d21960d7a
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-07-05 08:45:01 +0100

    feat: add initializeGroups func to create 12 Group entities on app startup refactor: rename createRole func to initializeRoles for better readability

--- src/main/java/com/bigdecimal/clasnapp/ClasnappApplication.java
+++ src/main/java/com/bigdecimal/clasnapp/ClasnappApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bigdecimal.clasnapp.group.Group;
+import com.bigdecimal.clasnapp.group.GroupName;
 import com.bigdecimal.clasnapp.group.GroupRepository;
 import com.bigdecimal.clasnapp.user.Role;
 import com.bigdecimal.clasnapp.user.RoleName;
@@ -27,12 +29,13 @@ public class ClasnappApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(RoleRepository roles, GroupRepository groups) {
 		return arg -> {
-			createRole(roles);
+			initializeRoles(roles);
+			initializeGroups(groups);
 		};
 	}
 
 	@Transactional
-	private void createRole(RoleRepository roles) {
+	private void initializeRoles(RoleRepository roles) {
 		List<Role> roleList = roles.findAll();
 		log.info("Number of existent roles is {}", roleList.size());
 		if (roleList.size() < 4) {
@@ -41,6 +44,28 @@ public class ClasnappApplication {
 			Role supervisor = roles.findByName(RoleName.SUPERVISOR).orElse(new Role(RoleName.SUPERVISOR));
 			Role md = roles.findByName(RoleName.MD).orElse(new Role(RoleName.MD));
 			roles.saveAll(Arrays.asList(user, groupLeader, supervisor, md));
+		}
+	}
+
+	@Transactional
+	private void initializeGroups(GroupRepository groups) {
+		List<Group> groupList = groups.findAll();
+		log.info("Number of existent groups is {}", groupList.size());
+		if (groupList.size() < 4) {
+			Group year1 = groups.findByName(GroupName.YEAR_1).orElse(new Group(GroupName.YEAR_1));
+			Group year2 = groups.findByName(GroupName.YEAR_2).orElse(new Group(GroupName.YEAR_2));
+			Group year3 = groups.findByName(GroupName.YEAR_3).orElse(new Group(GroupName.YEAR_3));
+			Group year4 = groups.findByName(GroupName.YEAR_4).orElse(new Group(GroupName.YEAR_4));
+			Group year5 = groups.findByName(GroupName.YEAR_5).orElse(new Group(GroupName.YEAR_5));
+			Group year6 = groups.findByName(GroupName.YEAR_6).orElse(new Group(GroupName.YEAR_6));
+			Group year7 = groups.findByName(GroupName.YEAR_7).orElse(new Group(GroupName.YEAR_7));
+			Group year8 = groups.findByName(GroupName.YEAR_8).orElse(new Group(GroupName.YEAR_8));
+			Group year9 = groups.findByName(GroupName.YEAR_9).orElse(new Group(GroupName.YEAR_9));
+			Group year10 = groups.findByName(GroupName.YEAR_10).orElse(new Group(GroupName.YEAR_10));
+			Group year11 = groups.findByName(GroupName.YEAR_11).orElse(new Group(GroupName.YEAR_11));
+			Group year12 = groups.findByName(GroupName.YEAR_12).orElse(new Group(GroupName.YEAR_12));
+			groups.saveAll(Arrays.asList(year1, year2, year3, year4, year5, year6, year7, year8, year9, year10, year11,
+					year12));
 		}
 	}
 }
