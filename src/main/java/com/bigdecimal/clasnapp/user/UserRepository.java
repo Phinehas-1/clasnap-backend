b96e275e094b8895f79cf15fcea25947c5d4a0c1
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-07-12 22:45:16 +0100

    feat: add findByUsername and findAllByGroup functionality to UserRepository

--- src/main/java/com/bigdecimal/clasnapp/user/UserRepository.java
+++ src/main/java/com/bigdecimal/clasnapp/user/UserRepository.java
@@ -1,8 +1,15 @@
 package com.bigdecimal.clasnapp.user;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.bigdecimal.clasnapp.group.Group;
+
 public interface UserRepository extends JpaRepository<User, UUID> {
+    public Optional<User> findByUsername(String username);
+
+    public List<User> findAllByGroup(Group group);
 }
