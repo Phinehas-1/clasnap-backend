f6f010303784e3ccca3ca7e5a979480edd4e6ba0
Phinehas Enakireru <phinehasenakireru@gmail.com>
2023-07-14 21:10:40 +0100

    refactor:  remove Report association mapping in Attendance. add FetchType.EAGER on User association mapping in Attendance

--- src/main/java/com/bigdecimal/clasnapp/report/attendance/Attendance.java
+++ src/main/java/com/bigdecimal/clasnapp/report/attendance/Attendance.java
@@ -1,21 +1,18 @@
 package com.bigdecimal.clasnapp.report.attendance;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import com.bigdecimal.clasnapp.report.Report;
 import com.bigdecimal.clasnapp.report.student.Student;
 import com.bigdecimal.clasnapp.user.User;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,25 +22,23 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Attendance {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(updatable = false)
-    private UUID id;
 
-    @ManyToOne
-    private User user;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(updatable = false)
+  private UUID id;
 
-    /* This will be a combination of the term name + week name */
-    @Column(nullable = false)
-    private String week;
+  @ManyToOne(fetch = FetchType.EAGER)
+  private User user;
 
-    @OneToOne(mappedBy = "attendance")
-    private Report report;
-    
-    @ManyToMany
-    private List<Student> students = new ArrayList<Student>();
+  /* This will be a combination of the term name + week name */
+  @Column(nullable = false)
+  private String week;
 
-    public Attendance(String week) {
-        this.week = week;
-    }  
+  @ManyToMany
+  private List<Student> students = new ArrayList<Student>();
+
+  public Attendance(String week) {
+    this.week = week;
+  }
 }
